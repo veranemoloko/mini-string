2541f4b526996bfc1a0542c2ba3c9d2cf09355e3
Verochka <133212611+veranemoloko@users.noreply.github.com>
2025-05-23 19:14:14 +0300

    Update s21_string.h

--- s21_string.h
+++ s21_string.h
@@ -8,5 +8,8 @@ int s21_strcmp(const char *s1, const char *s2);
 char *s21_strcpy(char *restrict dest, const char *restrict src);
 char *s21_strcat(char *restrict dest, const char *restrict src);
 const char *s21_strchr(const char *s, int c);
+void *s21_memcpy(void *restrict dest, const void *restrict src, size_t n);
+void *s21_memset(void *s, int c, size_t n);
+char *s21_strstr(const char *haystack, const char *needle);
 
 #endif 
